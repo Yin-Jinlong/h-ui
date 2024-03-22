a4e89bef1dfe25d4061a28d0a7bf17e19a003577
yjl <yjl_1567@qq.com>
2024-03-22 21:46:15 +0800

    添加card样式与全部样式索引

--- packages/components/card/src/Card.vue
+++ packages/components/card/src/Card.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="card">
-        <div class="card-header"
+    <div class="h-card">
+        <div class="h-card-header"
              data-empty-hide
              data-fill-width>
             <slot name="header"/>
@@ -14,37 +14,6 @@
         </div>
     </div>
 </template>
-
-<style lang="scss" scoped>
-@import '../../../style/tools/fns';
-
-.card {
-  background-color : var(--card-background);
-  border-radius    : 10px;
-  box-shadow       : 2px 2px 20px 0 rgba(0, 0, 0, 0.25);
-  margin           : 2em;
-  min-width        : 200px;
-  padding          : get-css(padding, xlarge);
-  width            : max-content;
-
-  & > div:not(:first-child) {
-    margin-top : get-css(padding, xlarge);
-  }
-
-}
-
-.card-header {
-  border-bottom : #222 solid 2px;
-  font-size     : 20px;
-  font-weight   : 900;
-}
-
-.card-content {
-  box-sizing : border-box;
-  width      : 100%;
-}
-
-</style>
 
 <script lang="ts" setup>
 
