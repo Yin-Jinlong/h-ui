408329437ffd2dacad8076e3759826962e25f5be
yjl <yjl_1567@qq.com>
2024-03-23 11:14:53 +0800

    deepAssign多个源

--- packages/utils/assign.ts
+++ packages/utils/assign.ts
@@ -1,4 +1,15 @@
-export function deepAssign<S = object, T = object>(target: T, source: S): S & T {
+/**
+ *
+ * 深度合并对象
+ *
+ * @description 合并sources到target，会忽略undefined的属性
+ *
+ * @param target 目标对象
+ * @param sources 源对象
+ *
+ * @author yjl
+ */
+export function deepAssign<S = object, T = object>(target: T, ...sources: S[]): S & T {
 
     function assign(target: any, source: any): any {
         for (const key in source) {
@@ -7,12 +18,15 @@ export function deepAssign<S = object, T = object>(target: T, source: S): S & T 
                 if (!target[key])
                     target[key] = {}
                 assign(target[key], value)
-            } else {
+            } else if (value) {
                 target[key] = value
             }
         }
         return target
     }
 
-    return assign(target, source) as S & T
+    for (const s of sources)
+        assign(target, s)
+
+    return target as S & T
 }
