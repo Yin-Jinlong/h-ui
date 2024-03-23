1ed2bd50619f1dd356a2d785be1b50d9d8474cc9
yjl <yjl_1567@qq.com>
2024-03-23 11:32:19 +0800

    优化生成button颜色css变量

--- packages/components/button/src/color-tool.ts
+++ packages/components/button/src/color-tool.ts
@@ -1,23 +1,42 @@
 import {DefinedNamedColor} from "../../../types"
-import chroma, {hsl} from "chroma-js"
+import chroma, {Color, hsl} from "chroma-js"
 
 const namedColors = ['primary', 'success', 'warning', 'danger', 'info', 'emphasize']
 
+/**
+ * 生成button颜色
+ *
+ * @description css变量
+ *
+ * @param color 颜色
+ * @param lightenCount 淡化数量
+ * @param darkenCount 加深数量
+ * @return css变量
+ *
+ * @author yjl
+ */
 export function genColor(color: string | DefinedNamedColor, lightenCount: number, darkenCount: number): string[] {
-    let btnColors = []
+    let btnColors: string[] = []
+
+    const add = (color: string | Color) => btnColors.push(color.toString())
+
     if (namedColors.includes(color)) {
-        btnColors.push(`var(--h-color-${color})`)
+        const addVar = (str: string=color) => add(`var(--h-color-${str})`)
+
+        addVar()
         for (let i = 1; i <= lightenCount; i++)
-            btnColors.push(`var(--h-color-${color}-${i})`)
+            addVar(`${color}-${i}`)
         for (let i = 1; i <= darkenCount; i++)
-            btnColors.push(`var(--h-color-${color}--${i})`)
+            addVar(`${color}--${i}`)
     } else {
         let c = chroma(color).hsl()
-        btnColors.push(hsl(...c).toString())
+        const addL = (dl: number = 0) => add(hsl(c[0], c[1], c[2] + dl))
+
+        addL()
         for (let i = 1; i <= lightenCount; i++)
-            btnColors.push(hsl(c[0], c[1], c[2] + 0.05 * i).toString())
+            addL(0.05 * i)
         for (let i = 1; i <= darkenCount; i++)
-            btnColors.push(hsl(c[0], c[1], c[2] - 0.05 * i).toString())
+            addL(-0.05 * i)
     }
     return btnColors
 }
