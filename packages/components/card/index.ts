611378eb0d60379d0def7bb4062ef3b0a8fb9bca
yjl <yjl_1567@qq.com>
2024-03-28 20:52:58 +0800

    优化代码

--- packages/components/card/index.ts
+++ packages/components/card/index.ts
@@ -1,4 +1,4 @@
-import Card from "./src/Card.vue"
+import Card from "./src/card.vue"
 
 export const HCard=Card;
 
--- packages/global.d.ts
+++ packages/global.d.ts
@@ -1,26 +1,25 @@
 // GlobalComponents for Volar
 declare module '@vue/runtime-core' {
-  export interface GlobalComponents {
-    HButton: typeof import('h-ui/es')['HButton']
-  }
+    export interface GlobalComponents {
+        HButton: typeof import('h-ui/es')['HButton']
+        HCard: typeof import('h-ui/es')['HCard']
+        HCheckBox: typeof import('h-ui/es')['HCheckBox']
+    }
 
-  interface ComponentCustomProperties {
-
-  }
 }
 
 declare global {
-  interface Document {
-    startViewTransition?: (callback: () => void | Promise<void>) => ViewTransition
-  }
+    interface Document {
+        startViewTransition?: (callback: () => void | Promise<void>) => ViewTransition
+    }
 
-  interface ViewTransition {
-    readonly finished: Promise<void>
-    readonly ready: Promise<void>
-    readonly updateCallbackDone: Promise<void>
+    interface ViewTransition {
+        readonly finished: Promise<void>
+        readonly ready: Promise<void>
+        readonly updateCallbackDone: Promise<void>
 
-    skipTransition(): void
-  }
+        skipTransition(): void
+    }
 
 }
 
