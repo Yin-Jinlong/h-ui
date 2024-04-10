57bbdf62dee4aa79d49becb7d42949e5b74b4336
yjl <yjl_1567@qq.com>
2024-04-10 21:49:15 +0800

    添加switch测试

---
+++ packages/components/switch/_TEST_/switch.test.ts
@@ -0,0 +1,33 @@
+import {render} from '@testing-library/vue'
+import {assertAttribute, assertHasClass, assertNoAttribute, element} from '@yin-jinlong/h-ui/_TEST_UTILS_'
+import {test} from 'vitest'
+
+import Switch from '../src/switch.vue'
+
+import '../style'
+
+test('switch', () => {
+    const card = render(Switch)
+    const ele = element(card)
+    assertHasClass(ele, 'h-switch')
+})
+
+test('switch not-checked', () => {
+    const card = render(Switch, {
+        props: {
+            modelValue: false,
+        }
+    })
+    const ele = element(card)
+    assertNoAttribute(ele, 'data-checked')
+})
+
+test('switch checked', () => {
+    const card = render(Switch, {
+        props: {
+            modelValue: true,
+        }
+    })
+    const ele = element(card)
+    assertAttribute(ele, 'data-checked')
+})
