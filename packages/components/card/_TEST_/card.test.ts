2e52f82320e8455453f1e02ad9923db3913abc3a
yjl <yjl_1567@qq.com>
2024-04-10 21:27:00 +0800

    添加card测试

---
+++ packages/components/card/_TEST_/card.test.ts
@@ -0,0 +1,71 @@
+import {render} from '@testing-library/vue'
+import {assertChildrenLength, assertHasClass, element} from '@yin-jinlong/h-ui/_TEST_UTILS_'
+import {test} from 'vitest'
+
+import Card from '../src/card.vue'
+
+import '../style'
+
+const CLASS_NAMES = ['h-card-header', 'h-card-line', 'h-card-content', 'h-card-line', 'h-card-footer']
+
+function check(ele: Element) {
+    assertHasClass(ele, 'h-card')
+}
+
+test('card', () => {
+    const card = render(Card, {
+        slots: {
+            header: () => 'test-header',
+            default: () => 'test',
+            footer: () => 'test-footer'
+        }
+    })
+    const ele = element(card)
+    check(ele)
+    assertChildrenLength(ele, 5)
+    Array.from(ele.children).forEach((c, i) => {
+        assertHasClass(c, CLASS_NAMES[i])
+    })
+})
+
+test('card no-header', () => {
+    const card = render(Card, {
+        slots: {
+            default: () => 'test',
+            footer: () => 'test-footer'
+        }
+    })
+    const ele = element(card)
+    check(ele)
+    assertChildrenLength(ele, 3)
+    Array.from(ele.children).forEach((c, i) => {
+        assertHasClass(c, CLASS_NAMES[i + 2])
+    })
+})
+
+test('card no-footer', () => {
+    const card = render(Card, {
+        slots: {
+            header: () => 'test-header',
+            default: () => 'test'
+        }
+    })
+    const ele = element(card)
+    check(ele)
+    assertChildrenLength(ele, 3)
+    Array.from(ele.children).forEach((c, i) => {
+        assertHasClass(c, CLASS_NAMES[i])
+    })
+})
+
+test('card only-content', () => {
+    const card = render(Card, {
+        slots: {
+            default: () => 'test'
+        }
+    })
+    const ele = element(card)
+    check(ele)
+    assertChildrenLength(ele, 1)
+    assertHasClass(ele.children[0], CLASS_NAMES[2])
+})
