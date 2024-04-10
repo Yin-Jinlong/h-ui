b6ef559544742157347d2f6661df8b1a4f11d36f
yjl <yjl_1567@qq.com>
2024-04-10 21:41:45 +0800

    添加判断属性

--- packages/_TEST_UTILS_/assert.ts
+++ packages/_TEST_UTILS_/assert.ts
@@ -1,4 +1,4 @@
-import {expect} from 'vitest'
+import {expect, assert} from 'vitest'
 
 export function assertChildrenLength(ele: Element, length: number) {
     expect(ele.children.length).toBe(length)
@@ -6,4 +6,12 @@ export function assertChildrenLength(ele: Element, length: number) {
 
 export function assertHasClass(ele: Element, c: string) {
     expect(Array.from(ele.classList)).contains(c)
+}
+
+export function assertAttribute(ele: Element, a: string) {
+    assert(ele.hasAttribute(a), `${a} is not exist`)
+}
+
+export function assertNoAttribute(ele: Element, a: string) {
+    assert(!ele.hasAttribute(a), `${a} is exist`)
 }
