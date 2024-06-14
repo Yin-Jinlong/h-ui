320ad51a345fc1b1bab90a938fae68ae6f443ba9
yjl <yjl_1567@qq.com>
2024-06-14 11:29:32 +0800

    docs(message): 更新文档，添加显示时长和关闭说明
    在消息组件的文档中，新增了关于显示时长和关闭功能的详细说明，包括时长的默认值和关闭时的错误处理。

--- docs/components/message/durations.vue
+++ docs/components/message/durations.vue
@@ -2,13 +2,30 @@
     <h-button @click="()=>{show('默认','primary')}" type="primary" color="primary">默认</h-button>
     <h-button @click="()=>{show('短','success',1000)}" type="primary" color="success">短</h-button>
     <h-button @click="()=>{show('长','emphasize',5000)}" type="primary" color="emphasize">长</h-button>
+    <br>
+    <div style="margin-top: 1rem">
+        <h-button @click="closeById" type="plain" color="danger">指定id关闭（最后一个消息）</h-button>
+    </div>
+    <div style="margin-top: 1rem">
+        <h-button @click="closeAll" type="primary" color="danger">关闭所有</h-button>
+    </div>
 </template>
 
 <script setup lang="ts">
 import {HButton, HMessage} from '@yin-jinlong/h-ui'
 
+let lastId = 0
+
 function show(msg: string, color: string, duration?: number) {
-    HMessage.show(msg, {color, duration})
+    lastId = HMessage.show(msg, {color, duration})
+}
+
+function closeById() {
+    HMessage.close(lastId)
+}
+
+function closeAll() {
+    HMessage.closeAll()
 }
 
 </script>
--- docs/components/message/index.md
+++ docs/components/message/index.md
@@ -8,6 +8,10 @@ title: H-UI-Message
 
 [.colors]
 
-## 显示时长
+## 显示时长&关闭
+
+时长为毫秒数字。默认为3000毫秒。
+
+关闭不存在的id时不会报错。
 
 [.durations]
