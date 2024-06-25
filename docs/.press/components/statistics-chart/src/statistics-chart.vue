e9cabfe9c7613418dff31ea45124e0d52377b62e
yjl <yjl_1567@qq.com>
2024-06-25 16:22:57 +0800

    docs(statistics-chart): 添加时间显示，移除近100条选项

--- docs/.press/components/statistics-chart/src/statistics-chart.vue
+++ docs/.press/components/statistics-chart/src/statistics-chart.vue
@@ -1,8 +1,8 @@
 <template>
     <div data-fill-size>
         <label data-flex-center style="justify-content: start">
-            只显示近100次
-            <h-check-box v-model="show100"/>
+            以时间显示
+            <h-check-box v-model="timeX"/>
         </label>
         <div ref="chartEle" class="chart"/>
     </div>
@@ -16,25 +16,27 @@
 </style>
 
 <script lang="ts" setup>
-import {isDark, HCheckBox} from '@yin-jinlong/h-ui'
+import {HCheckBox, isDark} from '@yin-jinlong/h-ui'
 import type {EChartsOption} from 'echarts'
-import * as echart from 'echarts/core'
+import {LineChart} from 'echarts/charts'
 import {
+    DataZoomComponent,
     GraphicComponent,
     GridComponent,
     LegendComponent,
     TitleComponent,
     TooltipComponent,
 } from 'echarts/components'
-import {LineChart} from 'echarts/charts'
+import * as echart from 'echarts/core'
 import {SVGRenderer} from 'echarts/renderers'
 // @ts-ignore
 import type {CallbackDataParams} from 'echarts/types/src/util/types'
 import {onMounted, ref, watch} from 'vue'
 
-import {lightTheme, darkTheme} from './theme'
+import {darkTheme, lightTheme} from './theme'
 
 echart.use([
+    DataZoomComponent,
     SVGRenderer,
     GraphicComponent,
     GridComponent,
@@ -44,38 +46,51 @@ echart.use([
     TooltipComponent,
 ])
 
-const show100 = ref(false)
+const timeX = ref(true)
 const chartEle = ref<HTMLDivElement>()
 
+type WithTime<T> = [Date, T]
+type OrWithTime<T> = WithTime<T> | [number, T]
 
 let data: any[] | undefined
 let c: echart.ECharts
 let x: number[]
 let hashes: string[]
-let files: number[]
-let lines: number[]
-let codes: number[]
+let times: Date[]
+let files: OrWithTime<number>[]
+let lines: OrWithTime<number>[]
+let codes: OrWithTime<number>[]
+
+function put<T>(list: OrWithTime<T>[], date: Date, value: T) {
+    if (timeX.value) {
+        list.push([date, value])
+    } else {
+        list.push([x.length, value])
+    }
+}
+
+async function convertData() {
+    data?.forEach((line, i) => {
+        if (line.length > 1) {
+            let [hash, time, file, blank, comment, code] = line
+            x.push(i + 1)
+            hashes.push(hash)
+            let date = new Date(time)
+            times.push(date)
+            put(files, date, parseInt(file))
+            put(lines, date, parseInt(blank) + parseInt(comment) + parseInt(code))
+            put(codes, date, parseInt(code))
+        }
+    })
+}
 
 async function getData() {
     x = []
     hashes = []
+    times = []
     files = []
     lines = []
     codes = []
-
-    async function convertData() {
-        let si = show100.value ? (data?.length ?? 0) - 101 : -1
-        data?.forEach((line, i) => {
-            if (i >= si && line.length > 1) {
-                let [hash, file, blank, comment, code] = line
-                x.push(i + 1)
-                hashes.push(hash)
-                files.push(parseInt(file))
-                lines.push(parseInt(blank) + parseInt(comment) + parseInt(code))
-                codes.push(parseInt(code))
-            }
-        })
-    }
 
     return data ? convertData() : fetch('/cloc.csv').then(res => res.text()).then(async (text) => {
         let [header, ..._data] = text.split('\n').map((line) => line.split(','))
@@ -99,11 +114,25 @@ function formatNum(num: string): string {
     return num.replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, '$1,')
 }
 
+function formatTimeZone(date: Date) {
+    let offset = date.getTimezoneOffset() / -60
+    return `GMT${offset >= 0 ? '+' : ''}${offset}`
+}
+
+function formatDate(date: Date) {
+    let dateStr = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+    let timeStr = `${date.getHours()}:${date.getMinutes()}`
+    return `${timeStr} ${dateStr} ${formatTimeZone(date)}`
+}
+
 function initChart() {
     if (c)
         c.dispose()
     c = echart.init(chartEle.value, isDark() ? darkTheme : lightTheme)
     c!.setOption({
+        grid: {
+            bottom: 80
+        },
         series: [
             {
                 data: files,
@@ -124,13 +153,16 @@ function initChart() {
         tooltip: {
             trigger: 'axis',
             formatter(args: CallbackDataParams[]) {
-                let s = '<table>'
+                let value = args[0].value[0]
+                let isDate = value instanceof Date
+                let date: Date = isDate ? value : times[value - 1]
+                let s = `<table><thead><tr><th>${isDate ? '' : (value + '.')} ${formatDate(date)}</thead><tbody>`
                 args.forEach((item, _) => {
                     let head = `${item.marker}${item.seriesName}`
                     s += `<tr>
                     <td align="left">${head}</td>
-                    <td align="right" style="padding: 0 0.25em"> ${item.seriesName == 'files' ? '' : formatNum(item.data.toString())}</td>
-                    <td align="right" style="padding: 0 0.25em"> <b>${formatFriendly(item.data.toString())}</b></td>
+                    <td align="right" style="padding: 0 0.25em"> ${item.seriesName == 'files' ? '' : formatNum(item.data[1].toString())}</td>
+                    <td align="right" style="padding: 0 0.25em"> <b>${formatFriendly(item.data[1].toString())}</b></td>
                     </tr>`
                 })
                 return s
@@ -142,9 +174,10 @@ function initChart() {
             icon: 'rect'
         },
         xAxis: {
-            name: 'commits',
-            type: 'category',
-            data: x
+            name: timeX.value ? 'time' : 'commits',
+            nameLocation: 'middle',
+            nameGap: 20,
+            type: timeX.value ? 'time' : 'category',
         },
         yAxis: [{
             type: 'value',
@@ -154,7 +187,15 @@ function initChart() {
             name: 'files',
             type: 'value',
             scale: true,
-        }]
+        }],
+        dataZoom: [
+            {
+                type: 'inside',
+                start: 0,
+                end: 100
+            },
+            {}
+        ],
     } as EChartsOption)
 
     c.on('dblclick', p => {
@@ -172,6 +213,6 @@ onMounted(() => {
     updateChart()
 })
 
-watch(show100, updateChart)
+watch(timeX, updateChart)
 
 </script>
