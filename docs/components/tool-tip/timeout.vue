58ff7e5a5796a230ea936b2821d1a6b577020f17
YJL <yjl_1567@qq.com>
2024-12-03 10:44:54 +0800

    doc: 添加时长

---
+++ docs/components/tool-tip/timeout.vue
@@ -0,0 +1,18 @@
+<template>
+    <h-tool-tip>
+        <h-button type="primary">默认</h-button>
+        <template #tip>
+            2s
+        </template>
+    </h-tool-tip>
+    <h-tool-tip :timeout="5000" style="margin-left: 1em">
+        <h-button type="primary">5s</h-button>
+        <template #tip>
+            5s
+        </template>
+    </h-tool-tip>
+</template>
+
+<script lang="ts" setup>
+import {HButton, HToolTip} from '@yin-jinlong/h-ui'
+</script>
